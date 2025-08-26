24fc8ca47f57974c728a7192847dc877e450c8e7
Ahmet9784 <ahmgoc080221@student.jenseneducation.se>
2025-08-26 11:16:41 +0200

    upgift 2

--- uppg2_welcome.py
+++ uppg2_welcome.py
@@ -5,3 +5,15 @@ Välkomna sedan användaren till programmet.
 Exempel på utskrift, det inom () ska ändras om variablerna ändras:
 Hej (Hampus), välkommen till programmet!
 """
+username = "ahmet"
+
+password = "lmaoooo"
+
+username = input("username")
+password = input("password")
+
+print(username)
+print(password)
+
+print(f"yo what up {username}, welcome to the program!")
+

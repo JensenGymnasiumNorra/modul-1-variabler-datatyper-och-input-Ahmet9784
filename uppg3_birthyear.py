45820fa239dde7ca911023367d6873ba79407d82
Ahmet9784 <ahmgoc080221@student.jenseneducation.se>
2025-08-26 11:35:09 +0200

    uppgift 3

--- uppg3_birthyear.py
+++ uppg3_birthyear.py
@@ -6,3 +6,16 @@ Exempel på utskrift, det inom () ska ändras om variablerna ändras:
 Du är (16) år gammal.
 
 """
+import datetime
+
+birth_year = 2008
+birth_month = 2
+birth_day = 21
+
+today = datetime.date(2025, 8, 26)
+
+birth_date= datetime.date(birth_year, birth_month, birth_day)
+
+age = today.year - birth_date.year
+
+print(f"You are {age} years old.")
